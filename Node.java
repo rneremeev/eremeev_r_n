5c5263d587387857911c59d853bf13dce8296753
IPRIT <ipritoflex@yandex.ru>
2016-06-22 11:03:39 +0300

    капли дождя

---
+++ Node.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eremeev_r_n;
+
+import org.opencv.core.Point;
+
+/**
+ *
+ * @author feels
+ */
+public class Node {
+    public Point point;
+    public Node next;
+}
